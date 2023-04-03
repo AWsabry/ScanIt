73192a1d23dfc622ce7140ada72eaea146e30d49
Abdelrahman Wael <55349570+AWsabry@users.noreply.github.com>
2023-04-03 14:53:13 +0200

    first commit

---
+++ Register_Login/admin.py
@@ -0,0 +1,41 @@
+from Register_Login.models import Profile, Team_Member
+from django.contrib import admin
+from firebase_admin import messaging
+
+# Register your models here.
+
+
+
+
+class Register(admin.ModelAdmin):
+    list_filter = ("email","first_name", "last_name", "last_modified")
+    list_display = ("email","first_name", 'last_name','last_modified','PhoneNumber','is_active','id'
+                  )
+    search_fields = ['email']
+
+
+
+class Team_Admin(admin.ModelAdmin):
+    model = Team_Member
+    list_display = ('first_name','last_name','email','PhoneNumber')
+
+
+class AccessTokenAdmin(admin.ModelAdmin):
+    model = Profile
+    fieldsets = (
+        (None, {"fields": (
+                'user', 'token', 'expires', 'created'
+            )}),
+    )
+    readonly_fields = ('token','created')
+    list_display = ('user', 'token', 'created')
+
+
+
+
+
+
+admin.site.register(Profile, Register)
+admin.site.register(Team_Member, Team_Admin)
+
+
